56844f71c20cc693c5e12e438f80f88b2329fc09
Ancuta Catalin Mihai <catalin_puiu11@yahoo.com>
2020-05-15 12:04:29 +0300

    "Adaugare pagina principala."

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,41 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 public class Main extends Application {
+    private Stage primaryStage;
+    private AnchorPane paginaA;
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AplicatieLibrarie");
 
+        PaginaAutentificare();
+    }
+
+    public void PaginaAutentificare() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("PaginaAutentificare.fxml"));
+            AnchorPane paginaA = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            Scene scene = new Scene(paginaA);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 }
