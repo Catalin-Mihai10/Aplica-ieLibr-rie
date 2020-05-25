b238650e7118a3edccd9d97a840f3043db029fe1
Tudorache Mihai - Ovidiu <ovi99or@gmail.com>
2020-05-25 17:25:36 +0300

    "Adaugare detalii carti"

--- src/main/java/Controllers/PaginaClient.java
+++ src/main/java/Controllers/PaginaClient.java
@@ -5,7 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
-import org.json.JSONException;
+import javafx.scene.control.TextArea;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -24,9 +24,13 @@ public class PaginaClient {
 
     @FXML
     private ListView<String> lista = new ListView<String>();
+    @FXML
+    private TextArea text;
     private ObservableList<String> observ = FXCollections.observableArrayList();
 
+
     public void afisareSF(ActionEvent actionEvent){
+        text.clear();
         lista.getItems().clear();
         JSONParser parser = new JSONParser();
         try (Reader reader = new FileReader("src/main/resources/Carti.json")) {
@@ -48,6 +52,7 @@ public class PaginaClient {
     }
 
     public void afisareActiune(ActionEvent actionEvent){
+        text.clear();
         lista.getItems().clear();
         JSONParser parser = new JSONParser();
         try (Reader reader = new FileReader("src/main/resources/Carti.json")) {
@@ -69,6 +74,7 @@ public class PaginaClient {
     }
 
     public void afisareDrama(ActionEvent actionEvent){
+        text.clear();
         lista.getItems().clear();
         JSONParser parser = new JSONParser();
         try (Reader reader = new FileReader("src/main/resources/Carti.json")) {
@@ -90,6 +96,7 @@ public class PaginaClient {
     }
 
     public void afisareHorror(ActionEvent actionEvent){
+        text.clear();
         lista.getItems().clear();
         JSONParser parser = new JSONParser();
         try (Reader reader = new FileReader("src/main/resources/Carti.json")) {
@@ -110,8 +117,8 @@ public class PaginaClient {
         }
     }
 
-
-    public void afisarePopulare(ActionEvent actionEvent) throws JSONException{
+    public void afisarePopulare(ActionEvent actionEvent) {
+        text.clear();
         lista.getItems().clear();
         ArrayList<JSONObject> list = new ArrayList<>();
         JSONParser parser = new JSONParser();
@@ -142,6 +149,28 @@ public class PaginaClient {
                 observ.add(sortat.get("Titlu:").toString());
             }
             lista.setItems(observ);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void afisareDetalii(){
+        String det = "";
+        JSONParser parser = new JSONParser();
+        try (Reader reader = new FileReader("src/main/resources/Carti.json")) {
+
+            JSONArray temp = (JSONArray) parser.parse(reader);
+            Iterator<JSONObject> it = temp.iterator();
+            while (it.hasNext()) {
+                JSONObject obiect = it.next();
+                if(obiect.get("Titlu:").toString().equals(lista.getSelectionModel().getSelectedItem())){
+                    det = "Titlu: "+obiect.get("Titlu:")+"\nAutor: "+obiect.get("Autor:")+"\nEditura:"
+                            +obiect.get("Editura: ")+"\nPret: "+obiect.get("Pret:")+" lei";
+                }
+            }
+            text.setText(det);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
