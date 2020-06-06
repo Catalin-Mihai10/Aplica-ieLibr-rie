c880def1f358a303a9777c02532f0b9cbe528f54
Tudorache Mihai - Ovidiu <ovi99or@gmail.com>
2020-06-06 16:56:07 +0300

    "Adaugare buton de adaugare in cos"

--- src/main/java/Controllers/CosDeCumparaturi.java
+++ src/main/java/Controllers/CosDeCumparaturi.java
@@ -1,4 +1,40 @@
 package Controllers;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CosDeCumparaturi {
+
+    @FXML
+    private ListView<String> listaCumparaturi = new ListView<>();
+
+    public void back(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("PaginaClient.fxml"));
+            AnchorPane paginaA = (AnchorPane) loader.load();
+            Scene scene = new Scene(paginaA);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setListaCumparaturi(ObservableList<String> observCos){
+        listaCumparaturi.getItems().clear();
+        listaCumparaturi.setItems(observCos);
+    }
 }
--- src/main/java/Controllers/PaginaClient.java
+++ src/main/java/Controllers/PaginaClient.java
@@ -1,20 +1,27 @@
 package Controllers;
 
+import Exceptii.NuSaAdaugatCarte;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
@@ -23,7 +30,7 @@ import java.util.Comparator;
 import java.util.Iterator;
 
 
-public class PaginaClient {
+public class PaginaClient extends CosDeCumparaturi{
 
     @FXML
     private ListView<String> lista = new ListView<String>();
@@ -31,9 +38,10 @@ public class PaginaClient {
     private TextArea text;
     @FXML
     private TextField baraCautare;
+    private JSONArray listaJson = new JSONArray();
 
     private ObservableList<String> observ = FXCollections.observableArrayList();
-
+    private static   ObservableList<String> observCos = FXCollections.observableArrayList();
 
     public void afisareSF(ActionEvent actionEvent){
         text.clear();
@@ -210,6 +218,65 @@ public class PaginaClient {
             }
         }
     }
+
+    private void CitesteFisier(){
+        observCos.clear();
+        JSONParser parser = new JSONParser();
+        try (Reader reader = new FileReader("src/main/resources/Cos.json")) {
+
+            JSONArray temp = (JSONArray) parser.parse(reader);
+            Iterator<JSONObject> it = temp.iterator();
+            while (it.hasNext()) {
+                JSONObject obj = it.next();
+                listaJson.add(obj);
+                if(obj.get("Username:").equals(PaginaLogIn.getNume())) {
+                    //listaJson.add(obj);
+                    observCos.add(obj.get("Titlu:").toString());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void adaugareInCos(){
+        CitesteFisier();
+        JSONObject obiect = new JSONObject();
+        obiect.put("Username:", PaginaLogIn.getNume());
+        obiect.put("Titlu:", lista.getSelectionModel().getSelectedItem());
+
+        try (FileWriter fisier = new FileWriter("src/main/resources/Cos.json")) {
+            listaJson.add(obiect);
+            fisier.write(listaJson.toJSONString());
+            fisier.flush();
+        } catch (IOException e) {
+            throw new NuSaAdaugatCarte();
+        }
+    }
+
+    public void vizualizareCos(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("CosDeCumparaturi.fxml"));
+            AnchorPane paginaA = (AnchorPane) loader.load();
+            CosDeCumparaturi controller = loader.getController();
+            CitesteFisier();
+            controller.setListaCumparaturi(observCos);
+            Scene scene = new Scene(paginaA);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
 
 
