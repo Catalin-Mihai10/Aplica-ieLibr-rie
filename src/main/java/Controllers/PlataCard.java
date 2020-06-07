62a546f0798fff35e82a907b0e629216567c8a3b
Ancuta Catalin Mihai <catalin_puiu11@yahoo.com>
2020-06-07 14:09:22 +0300

    "Verificare detalii card"

--- src/main/java/Controllers/PlataCard.java
+++ src/main/java/Controllers/PlataCard.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Exceptii.DetaliiCardIncorecte;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,6 +11,9 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class PlataCard {
     @FXML
@@ -38,18 +42,33 @@ public class PlataCard {
     }
 
     public void finalizare(ActionEvent actionEvent) {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getClassLoader().getResource("ComandaFinalizata.fxml"));
-            AnchorPane paginaA = (AnchorPane) loader.load();
-            Scene scene = new Scene(paginaA);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        if(verificare() == 1) {
+            try {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getClassLoader().getResource("ComandaFinalizata.fxml"));
+                AnchorPane paginaA = (AnchorPane) loader.load();
+                Scene scene = new Scene(paginaA);
+                Stage stage = new Stage();
+                stage.setScene(scene);
+                stage.show();
+                ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            throw new DetaliiCardIncorecte();
         }
-        catch (IOException e) {
-            e.printStackTrace();
+    }
+
+    public int verificare(){
+        Calendar data1 = Calendar.getInstance();
+        data1.setTime(new Date());
+        if(text1.getText().length() != 16 || (Integer.parseInt(text3.getText()) < data1.get(Calendar.YEAR))) return 0;
+        else if((Integer.parseInt(text3.getText()) == data1.get(Calendar.YEAR))){
+            if(Integer.parseInt(text2.getText()) < data1.get(Calendar.MONTH)) return 0;
         }
+        else if(text1.getText() == null || text2.getText() == null || text3.getText() == null
+                || text4.getText() == null) return 0;
+        return 1;
     }
 }
---
+++ src/main/java/Exceptii/DetaliiCardIncorecte.java
@@ -0,0 +1,8 @@
+package Exceptii;
+
+public class DetaliiCardIncorecte extends RuntimeException{
+
+    public DetaliiCardIncorecte(){
+        super(String.format("Datele introduse sunt incorecte!"));
+    }
+}
