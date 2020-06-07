1cab26083058df0ac2b3f56e577f3358db6c73e5
Tudorache Mihai - Ovidiu <ovi99or@gmail.com>
2020-06-07 12:49:21 +0300

    "Adaugare buton confirma comanda"

--- src/main/java/Controllers/CosDeCumparaturi.java
+++ src/main/java/Controllers/CosDeCumparaturi.java
@@ -48,6 +48,22 @@ public class CosDeCumparaturi {
         }
     }
 
+    public void confirma(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("MetodePlata.fxml"));
+            AnchorPane paginaA = (AnchorPane) loader.load();
+            Scene scene = new Scene(paginaA);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+            ((Node)(actionEvent.getSource())).getScene().getWindow().hide();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void setListaCumparaturi(ObservableList<String> observCos){
         listaCumparaturi.getItems().clear();
         listaCumparaturi.setItems(observCos);
