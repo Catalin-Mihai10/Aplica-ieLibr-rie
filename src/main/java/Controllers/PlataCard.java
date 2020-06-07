686dd1331d4038fe30fe66b24e08ce29b222cc42
Tudorache Mihai - Ovidiu <ovi99or@gmail.com>
2020-06-07 13:19:39 +0300

    "Adaugare pagina detalii plata si buton finalizare plata"

--- src/main/java/Controllers/PlataCard.java
+++ src/main/java/Controllers/PlataCard.java
@@ -1,36 +1,30 @@
 package Controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MetodePlata {
-
+public class PlataCard {
+    @FXML
+    private TextField text1;
+    @FXML
+    private TextField text2;
+    @FXML
+    private TextField text3;
+    @FXML
+    private TextField text4;
 
     public void back(ActionEvent actionEvent) {
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getClassLoader().getResource("CosDeCumparaturi.fxml"));
-            AnchorPane paginaA = (AnchorPane) loader.load();
-            Scene scene = new Scene(paginaA);
-            Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-            ((Node) (actionEvent.getSource())).getScene().getWindow().hide();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void plataCard(ActionEvent actionEvent) {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getClassLoader().getResource("PlataCard.fxml"));
+            loader.setLocation(getClass().getClassLoader().getResource("MetodePlata.fxml"));
             AnchorPane paginaA = (AnchorPane) loader.load();
             Scene scene = new Scene(paginaA);
             Stage stage = new Stage();
